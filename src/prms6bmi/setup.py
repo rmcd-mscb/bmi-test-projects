8a7731b809847494e8288cba3f65e98971e9862a
Richard R. McDonald <rmcd@usgs.gov>
2020-02-24 17:17:39 -0700

    some reconfigureing

--- src/prms6bmi/setup.py
+++ src/prms6bmi/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='prms6-bmi',
+setup(name='prms6bmi',
       version='0.1',
       description='Utilities for working with prms6-bmi I/O',
       url='https://github.com/rmcd-mscb/bmi-test-projects',
       author='rmcd-mscb',
       author_email='rmcd@usgs.gov',
       license='public domain',
-      packages=['prms6-bmi'],
+      packages=['prms6bmi'],
       zip_safe=False)
